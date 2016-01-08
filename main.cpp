6d7e498dc5dddb8b37ce5a8ef9a63049b9dabcdb
iryna <iraklepak@gmail.com>
2016-01-08 23:54:59 +0200

    first

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include<QPushButton>
+#include<QScrollBar>
+#include<QSlider>
+#include <QLabel>
 int main(int argc, char *argv[])
 {
     QApplication prog(argc, argv);
-   QPushButton* button = new QPushButton("Close");
-   QObject::connect(button,SIGNAL(clicked()),&prog,SLOT(quit()));
-   button->show();
+//   QPushButton* button = new QPushButton("Close");
+//   QObject::connect(button,SIGNAL(clicked()),&prog,SLOT(quit()));
+//   button->show();
+    QPushButton* WhoIAm = new QPushButton("Who I AM?");
+    QLabel* IAm = new QLabel("???");
+
+    QObject::connect(WhoIAm,SIGNAL(clicked()),&prog,SLOT(IAm->setText(" I am very cool project")));
+
     return prog.exec();
 
 }
